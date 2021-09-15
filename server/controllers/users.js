333b9ff7337acd1c35686f092c5f9a51e78cc866
axtc <axtc@student.chalmers.se>
2021-09-15 08:19:46 +0200

    User schema created inside user.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    _id: {type: String},
+    name: {type: String},
+    password: {type: String}
+});
+
+var User = mongoose.model('users', userSchema);
 
 router.post('/api/users', function(req, res, next){
-    var user = {
+    var user = new User({
         "_id": 0,
         "name": "insertName",
         "password": "password"
-    }
+    })
+    user.save(function(err, user){
+        if(err){return console.error(err);}
+        res.status(201).json(user);
+    })
     res.status(201).json(user);
 });
 
