c5087f9878934ac8cc46cce63a9914aa8042b740
Hannah Maltkvist <hanmalt@student.chalmers.se>
2021-09-16 08:47:38 +0000

    Remove userID attribute

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    user_id: {type: String},
     name: {type: String},
     password: {type: String}
 });
