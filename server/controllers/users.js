da2983b9e109c1b18f07ffbcb68d02ea0534d0df
iachella <iachella@student.chalmers.se>
2021-09-16 14:36:00 +0200

    add get all users, add delete all users, specify status codes for all user related requests

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,10 +4,28 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.post('/api/users', function (req, res, next) {
+    
     var user = new User(req.body);
     user.save(function (err) {
         if (err) { return next(err); }
         res.status(201).json(user);
+    });
+});
+
+router.get('/api/users', function (req, res, next){
+
+    User.find(function(err, users){
+        if (err) { return next(err); }
+        return res.status(200).json({"users": users});
+    });
+});
+
+router.delete('/api/users', function (req, res, next){
+
+    User.deleteMany(function(err, users){
+        if (err) { return next(err); }
+        //TODO: change res message
+        return res.status(202).json({"users": users});
     });
 });
 
@@ -21,10 +39,12 @@ router.get('/api/users/:id', function (req, res, next){
             return res.status(404).json({"message": "User not found"});
         }
         
-        var returndUser = user;
-        res.json(returndUser);
+        var returnedUser = user;
+        return res.status(200).json(returnedUser);
     })
 })
+
+//put and patch
 
 router.delete('/api/users/:id', function(req, res, next) {
     
@@ -35,7 +55,7 @@ router.delete('/api/users/:id', function(req, res, next) {
         if (user == null) {
             return res.status(404).json({"message": "User not found"});
         }
-        res.json(user);
+        return res.status(202).json(user);
     });
 });
 
