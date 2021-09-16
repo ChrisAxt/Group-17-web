35d18fa94b4bcdf1f9f218e689faea60f41bb384
Hannah Maltkvist <hanmalt@student.chalmers.se>
2021-09-16 08:49:45 +0000

    Fix user routing

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-router.post('/api/users', function(req, res, next){
+router.post('/', function (req, res, next) {
     var user = new User(req.body);
-    user.save(function(err, user){
-        if(err){return next(err);}
+    user.save(function (err) {
+        if (err) { return next(err); }
         res.status(201).json(user);
-    })
+    });
 });
+
 
 /* router.delete('/api/users/:user_Id', function(req, res, next){
     
