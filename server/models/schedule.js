9b3e06d01218ecd447b2bfaa8852b5853e9ee3d4
iachella <iachella@student.chalmers.se>
2021-09-20 10:50:19 +0200

    add relationships according to updated ER diagram

--- server/models/schedule.js
+++ server/models/schedule.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var scheduleSchema = new Schema({
+    location: {type: String},
     time: {type: String},
     day: {type: String},
     week: {type: Number},
     month: {type: String},
     year: {type: Number}
- 
 });
 
 module.exports = mongoose.model('schedule', scheduleSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,10 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
     user_id: {type: String},
     name: {type: String},
-    password: {type: String}
+    password: {type: String},
+    createdClubIds: {type: Array}, 
+    clubIds: {type: Array},
+    eventIds: {type: Array}
 });
 
 module.exports = mongoose.model('users', userSchema);
