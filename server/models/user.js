1db206a585de75d5d70cbe6ff5b91a978e304787
iachella <iachella@student.chalmers.se>
2021-09-20 13:44:13 +0200

    add put and patch for users

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ var userSchema = new Schema({
     user_id: {type: String},
     name: {type: String},
     password: {type: String},
-    createdClubIds: {type: Array}, 
+    createdClubIds: {type: Array},
     clubIds: {type: Array},
     eventIds: {type: Array}
 });
