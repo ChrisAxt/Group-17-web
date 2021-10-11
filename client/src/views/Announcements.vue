75438474b08c6886f9890e6f38ca260150002531
iachella <iachella@student.chalmers.se>
2021-10-11 16:26:36 +0200

    announcements methods and basic data

--- client/src/views/Announcements.vue
+++ client/src/views/Announcements.vue
@@ -14,8 +14,43 @@
     </body>
 </template>
 <script>
-import AnnouncementsObject from '@/components/AnnouncementsObject.vue'
+import { Api } from '@/Api'
 import MainNavbar from '@/components/MainNavbar.vue'
+import AnnouncementsObject from '@/components/AnnouncementsObject.vue'
 
-export default ({ components: { AnnouncementsObject, MainNavbar } })
+export default ({
+  name: 'announcements',
+  components: { AnnouncementsObject, MainNavbar },
+  mounted() {
+    console.log('page is loaded!')
+    Api.get('/announcements')
+      .then(response => {
+        console.log(response)
+        this.announcements = response.data.announcements
+      })
+      .catch(error => {
+        this.clubs = []
+        console.log(error)
+      })
+      .then(() => {
+        console.log('This runs every time after success or error')
+      })
+  },
+
+  methods: {
+    deleteAnnouncements() {
+      console.log('Delete all announcements')
+      Api.delete('/announcements')
+        .then(response => {
+          console.log(response)
+          this.announcements = response.data.announcements
+        })
+    }
+  },
+  data() {
+    return {
+      clubs: []
+    }
+  }
+})
 </script>
